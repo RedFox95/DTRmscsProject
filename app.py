dad90a0ebb9dc97f44b1d3919f291ee9fe167c83
Dale Doster <dale.doster1@marist.edu>
2024-04-11 13:43:01 -0400

    Converted timestamp to date time and added process names to reports

--- app.py
+++ app.py
@@ -65,11 +65,11 @@ def reports():
         processes_ids = set()
         processes_ids |= { int(x[0]) for x in process_metrics }
         processes_ids = list(processes_ids)
-        processes = db.get_process_by_id(processes_ids)
+        processes = {process[0]: process[1] for process in db.get_process_by_id(processes_ids)}
         return render_template('report.html', systemMetrics=system_metrics, processMetrics=process_metrics, processes=processes)
 
     # This route renders the HTML template for the report view.
-    return render_template('report.html', systemMetrics="", processMetrics="", processes="")
+    return render_template('report.html', systemMetrics="undefined", processMetrics="undefined", processes="undefined")
 
 @app.route('/api/metrics/realtime', methods=['GET'])
 def all_api_metrics():
